2879a7769e62dba8a3136296b638b64eeec44028
also known as Anugra Pratama Rameli <salmanrameli@hotmail.com>
2021-01-29 15:35:05 +0700

    tambah asset + isi website

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -71,39 +71,314 @@
                 </div>
             </div>
         </nav>
-        <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <p class="hero-title">PHP Hosting</p>
+                    <p class="hero-subtitle">Cepat, handal, penuh dengen modul PHP yang Anda butuhkan</p>
+                    <p>Solusi PHP untuk performa query yang lebih cepat</p>
+                    <p>Konsumsi memori yang lebih rendah.</p>
+                    <p>Support PHP 5.3, PHP 5.4, PHP 5.5, PHP 5.6, PHP 7</p>
+                    <p>Fitur enkripsi IonCube dan Zend Guard Loaders</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <img src="{{ asset('/svg/illustration-banner-PHP-hosting-01.svg') }}" alt="illustration-banner-php-hosting-01">
+                </div>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-3">
+                    <img src="{{ asset('/svg/zendguard.svg') }}" alt="zendguard">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset('/svg/composer.svg') }}" alt="composer">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset('/svg/ioncube.svg') }}" alt="ioncube">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 px-0">
+                    <p class="hero-title text-center">Paket Hosting Singapura yang Tepat</p>
+                    <p class="hero-subtitle text-center">Diskon 40% + Domain dan SSL Gratis untuk Anda</p>
+                </div>
+                <div class="col-12 col-md-3 px-0">
+                    <p class="hero-title-small text-center border pb-3 mb-0">Bayi</p>
+                    <div class="border border-top-0">
+                        <p><del>Rp 19.900</del></p>
+                        <p>Rp 14.900/bln</p>
+                    </div>
+                    <div class="border border-top-0 p-3">
+                        <p class="mb-0">938 Pengguna Terdaftar</p>
+                    </div>
+                    <div class="border border-top-0 p-3">
+                        <p><b>0.5X RESOURCE POWER</b></p>
+                        <p><b>500 MB</b> Disk Space</p>
+                        <p><b>Unlimited</b> Bandwidth</p>
+                        <p><b>Unlimited</b> Databases</p>
+                        <p><b>1</b> Domain</p>
+                        <p><b>Instant</b> Backup</p>
+                        <p><b>Unlimited</b> SSL Gratis Selamanya</p>
+                        <button class="btn">Pilih Sekarang</button>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3 px-0">
+                    <p class="hero-title-small text-center border pb-3 mb-0">Pelajar</p>
+                    <div class="border border-top-0">
+                        <p><del>Rp 46.900</del></p>
+                        <p>Rp 23.450/bln</p>
+                    </div>
+                    <div class="border border-top-0 p-3">
+                        <p class="mb-0">4.168 Pengguna Terdaftar</p>
+                    </div>
+                    <div class="border border-top-0 p-3">
+                        <p><b>1X RESOURCE POWER</b></p>
+                        <p><b>Unlimited</b> Disk Space</p>
+                        <p><b>Unlimited</b> Bandwidth</p>
+                        <p><b>Unlimited</b> POP3 Email</p>
+                        <p><b>Unlimited</b> Databases</p>
+                        <p><b>10</b> Addon Domains</p>
+                        <p><b>Instant</b> Backup</p>
+                        <p><b>Domain</b> Gratis Selamanya</p>
+                        <p><b>Unlimited</b> SSL Gratis Selamanya</p>
+                        <button class="btn">Pilih Sekarang</button>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3 px-0">
+                    <p class="hero-title-small text-center border pb-3 mb-0">Personal</p>
+                    <div class="border border-top-0">
+                        <p><del>Rp 58.900</del></p>
+                        <p>Rp 38.900/bln</p>
+                    </div>
+                    <div class="border border-top-0 p-3">
+                        <p class="mb-0">10.017 Pengguna Terdaftar</p>
+                    </div>
+                    <div class="border border-top-0 p-3">
+                        <p>2X RESOURCE POWER</p>
+                        <p><b>Unlimited</b> Disk Space</p>
+                        <p><b>Unlimited</b> Bandwidth</p>
+                        <p><b>Unlimited</b> POP3 Email</p>
+                        <p><b>Unlimited</b> Databases</p>
+                        <p><b>Unlimited</b> Addon Domains</p>
+                        <p><b>Instant</b> Backup</p>
+                        <p><b>Domain</b> Gratis Selamanya</p>
+                        <p><b>Unlimited</b> SSL Gratis Selamanya</p>
+                        <p><b>Private</b> Name Server</p>
+                        <p><b>SpamAssasin</b> Mail Protection</p>
+                        <button class="btn">Pilih Sekarang</button>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3 px-0">
+                    <p class="hero-title-small text-center border pb-3 mb-0">Bisnis</p>
+                    <div class="border border-top-0">
+                        <p><del>Rp 109.900</del></p>
+                        <p>Rp 65.900/bln</p>
+                    </div>
+                    <div class="border border-top-0 p-3">
+                        <p class="mb-0">3.552 Pengguna Terdaftar</p>
+                    </div>
+                    <div class="border border-top-0 p-3">
+                        <p>3X RESOURCE POWER</p>
+                        <p><b>Unlimited</b> Disk Space</p>
+                        <p><b>Unlimited</b> Bandwidth</p>
+                        <p><b>Unlimited</b> POP3 Email</p>
+                        <p><b>Unlimited</b> Databases</p>
+                        <p><b>Unlimited</b> Addon Domains</p>
+                        <p><b>Magic Auto</b> Backup & Restore</p>
+                        <p><b>Domain</b> Gratis Selamanya</p>
+                        <p><b>Unlimited</b> SSL Gratis Selamanya</p>
+                        <p><b>Private</b> Name Server</p>
+                        <p><b>Prioritas</b> Layanan Support</p>
+                        <p><b>SpamExpert</b> Pro Mail Protection</p>
+                        <button class="btn">Pilih Sekarang</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <section id="hero-promotion">
-
-                    </section>
-                    <section id="dependency-managers">
-
-                    </section>
-                    <section id="pilihan-paket">
-
-                    </section>
-                    <section id="limit-php">
-
-                    </section>
-                    <section id="fitur-hosting">
-
-                    </section>
-                    <section id="promo-laravel">
-
-                    </section>
-                    <section id="list-modul-php">
-
-                    </section>
-                    <section id="linux-hosting">
-
-                    </section>
+                    <p class="hero-subtitle text-center">Powerful dengan Limit PHP yang Lebih Besar</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <table class="table table-striped border">
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td>max execution time 300s</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>max execution time 300s</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>php memory limit 1024 MB</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-12 col-md-6">
+                    <table class="table table-striped border">
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td>post max size 128 MB</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>upload max filesize 128 MB</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>max input vars 2500</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="hero-title text-center">Semua Paket Hosting Sudah Termasuk</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('/svg/php.svg')  }}" alt="php">
+                    <p class="bold">PHP Semua Versi</p>
+                    <p class="text">Pilih mulai dari versi PHP 5.3 s/d PHP 7. Ubah sesuka Anda!</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('/svg/db.svg')  }}" alt="db">
+                    <p class="bold">MySQL Versi 5.6</p>
+                    <p class="text">Nikmati MySQL versi terbau, tercepat dan kaya akan fitur.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('/svg/cpanel.svg')  }}" alt="cpanel">
+                    <p class="bold">Panel Hosting cPanel</p>
+                    <p class="text">Kelola website dengan panel canggih yang familiar di hati Anda.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('/svg/shield.svg')  }}" alt="shield">
+                    <p class="bold">Garansi Uptime 99.9%</p>
+                    <p class="text">Data center yang mendukung kelangsungan website Anda 24/7.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('/svg/innodb.svg')  }}" alt="innodb">
+                    <p class="bold">Database InnoDB Unlimited</p>
+                    <p class="text">Jumlah dan ukuran database yang tumbuh sesuai kebutuhan Anda.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="{{ asset('/svg/db-remote.svg')  }}" alt="db-remote">
+                    <p class="bold">Wildcard Remote MySQL</p>
+                    <p class="text">Mendukung s/d 25 max_user_connections dan 100 max_connections.</p>
                 </div>
                 <div class="col-12">
-
+                    <p class="hero-title text-center">Mendukung Penuh Framework Laravel</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <p class="hero-subtitle">Tak perlu menggunakan dedicated server ataupun VPS yang mahal. Layanan PHP hosting murah kami mendukung penuh framework favorit Anda</p>
+                    <p>Install Laravel 1 klik dengan Softaculous Installer.</p>
+                    <p>Mendukung ekstensi <b>PHP, MCrypt, phar, mbstring, json,</b> dan <b>fileinfo</b>.</p>
+                    <p>Tersedia <b>Composer</b> dan <b>SSH</b> untuk menginstal packages pilihan Anda.</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <img src="{{ asset('/svg/banner-laravel.svg') }}" alt="banner-laravel">
+                </div>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 text-center">
+                    <p class="hero-subtitle">Modul Lengkap untuk Menjalankan Aplikasi PHP Anda.</p>
+                </div>
+                <div class="col-12 col-md-3">
+                    IcePHP<br>
+                    apc<br>
+                    apcu<br>
+                    apm<br>
+                    ares<br>
+                    bcmath<br>
+                    bcompiler<br>
+                    big_int<br>
+                    bitset<br>
+                    bloomy<br>
+                    bz2_filter<br>
+                    clamav<br>
+                    coin_acceptor<br>
+                    crack<br>
+                    dba
+                </div>
+                <div class="col-12 col-md-3">
+                    http<br>
+                    huffman<br>
+                    idn<br>
+                    igbinary<br>
+                    imagick<br>
+                    imap<br>
+                    inclued<br>
+                    inotify<br>
+                    interbase<br>
+                    intl<br>
+                    ioncube_loader<br>
+                    ioncube_loader_4<br>
+                    jsmin<br>
+                    json<br>
+                    ldap
+                </div>
+                <div class="col-12 col-md-3">
+                    nd_pdo_mysql<br>
+                    oauth<br>
+                    oci8<br>
+                    odbc<br>
+                    opcache<br>
+                    pdf<br>
+                    pdo<br>
+                    pdo_dlblib<br>
+                    pdo_firebird<br>
+                    pdo_mysql<br>
+                    pdo_odbc<br>
+                    pdo_pgsql<br>
+                    pdo_sqlite<br>
+                    pgsql<br>
+                    phalcon
+                </div>
+                <div class="col-12 col-md-3">
+                    stats<br>
+                    stem<br>
+                    stomp<br>
+                    suhosin<br>
+                    sybase_ct<br>
+                    sysvmsg<br>
+                    sysvsem<br>
+                    sysvshm<br>
+                    tidy<br>
+                    timezonedb<br>
+                    trader<br>
+                    translit<br>
+                    uploadprogress<br>
+                    uri_template<br>
+                    uuid
                 </div>
                 <div class="col-12">
-
+                    <button class="btn">Selengkapnya</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <p class="hero-subtitle">Linux Hosting yang Stabil dengan Teknologi LVE</p>
+                    <p>SuperMicro <b>Intel Xeon 24-Cores</b> server dengan RAM <b>128 GB</b> dan teknologi <b>LVE CloudLinux</b> untuk stabilitas server Anda. Dilengkapi dengan <b>SSD</b> untuk
+                    kecepatan <b>MySQL</b> dan caching, Apache load balancer berbasis LiteSpeed Technologies, <b>CageFS</b> security, <b>Raid-10</b> protection dan auto backup untuk keamanan website PHP Anda.
+                    </p>
+                    <button class="btn">Pilih Hoting Anda</button>
+                </div>
+                <div class="col-12 col-md-6">
+                    <img src="{{ asset('/support.png') }}" alt="support">
                 </div>
             </div>
         </div>
